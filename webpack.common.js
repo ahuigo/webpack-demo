454e8b1ec8542b0457d70fa16681188e689eb613
Xuehui Ye <yexuehui@momenta.ai>
2019-03-29 23:03:49 +0800

    split-chunk

--- webpack.common.js
+++ webpack.common.js
@@ -4,14 +4,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
+    another: './src/another-module.js',
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Production'
     })
   ],
+  optimization: {
+  splitChunks: {
+      chunks: 'all'
+    }
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
