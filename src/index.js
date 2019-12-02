8512728f01ac7366b54e07f9976cd934f6d377a5
Xuehui Ye <yexuehui@momenta.ai>
2019-12-03 07:32:48 +0800

    1.webpack-start

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import printMe from './print.js';
+    function component() {
+        var element = document.createElement('div');
+        element.innerHTML = 'Hello'+ 'webpack';
 
-function component() {
-  console.log(Data);
-  let element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-   var btn = document.createElement('button');
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   btn.innerHTML = '点击这里，查看 console！';
-   btn.onclick = printMe;
-   element.appendChild(btn);
-   element.classList.add('hello');
-
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
+        return element;
+    }
+    document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,9 @@
+    const path = require('path');
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ title: '管理输出' }),
-    new CleanWebpackPlugin(),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: __dirname + '/dist',
-    publicPath: '/',
-  },
-  devServer: {
-    contentBase: './dist'
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      //oader 从右到左地取值(evaluate)/执行(execute)。在下面的示例中，从 sass-loader 开始执行，然后继续执行 css-loader，最后以 style-loader 为结束。
-      { test: /\.ts$/, use: 'ts-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-      { test: /\.(png|svg|jpg|gif)$/, use: [ 'file-loader' ] },
-      { test: /\.(csv|tsv)$/, use: [ 'csv-loader' ] },
-      { test: /\.xml$/, use: [ 'xml-loader' ] }
-    ]
-  },
-  mode: "none",
-};
+    module.exports = {
+      entry: './src/index.js',
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+      }
+    };
