0c62745e6556e997a614899c08feffe9d79e4c17
Xuehui Ye <yexuehui@momenta.ai>
2019-12-03 09:19:00 +0800

    3.entry-output

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-    function component() {
-        var element = document.createElement('div');
-        element.innerHTML = 'Hello'+ 'webpack';
+  import _ from 'lodash';
+ import printMe from './print.js';
 
-        return element;
-    }
-    document.body.appendChild(component());
+ function component() {
+   var element = document.createElement('div');
+   var btn = document.createElement('button');
+
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+   btn.innerHTML = 'Click me and check the console!';
+   btn.onclick = printMe;
+
+   element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
+
