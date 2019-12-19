140fd5b0cd936eb71ab2e98d830942648dc38b1c
Xuehui Ye <yexuehui@momenta.ai>
2019-12-20 01:00:31 +0800

    process.env.APP_ENV

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import conf from './ref.json';
 
 export default function hello() {
 	console.log(conf)
+	console.log(process.env.APP_ENV)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
+    var webpack = require('webpack');
     const path = require('path');
 
     module.exports = {
-      entry: './src/index.js',
-    mode:'development',
-      output: {
-        filename: 'hello.js',
-        path: path.resolve(__dirname, 'dist'),
-       library: 'hello',
-       libraryTarget: 'umd',
-      globalObject: 'this',
-       //libraryTarget: 'commonjs',
+        entry: './src/index.js',
+        mode: 'development',
+        output: {
+            filename: 'hello.js',
+            path: path.resolve(__dirname, 'dist'),
+            library: 'hello',
+            libraryTarget: 'umd',
+            globalObject: 'this',
 
-      },
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.APP_ENV': JSON.stringify(process.env.APP_ENV),
+                'APP_VERSION': '"3.1.1"',
+            })
+        ]
         /*
         module: {
             rules: [
